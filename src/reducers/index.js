7db678fd5cb128e79526f2acce6ca5bd51b9f180
Colin Rupp <colin@thinkful.com>
2018-10-15 11:50:57 -0700

    added reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,31 @@
+import {
+  FETCH_RECIPES_REQUEST,
+  FETCH_RECIPES_SUCCESS,
+  FETCH_RECIPES_ERROR
+} from '../actions/recipes.js';
+
+const initialState = {
+  hits: [],
+  loading: false,
+  error: null
+}
+
+export const recipeReducer = (state = initialState, action) => {
+  if (action.typ === FETCH_RECIPES_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true
+    })
+  } else if (action.type === FETCH_RECIPES_SUCCESS) {
+    return Object.assign({}, state, {
+      hits: action.hits,
+      loading: false
+    })
+  } else if (action.type === FETCH_RECIPES_ERROR) {
+    return Object.assign({}, state, {
+      loading: false,
+      error: action.error
+    })
+  } else {
+    return state
+  }
+}
