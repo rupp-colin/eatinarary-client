d69ef4553c4bd2ea5cb205617b130d992da48295
Colin Rupp <colin@thinkful.com>
2018-10-16 14:11:08 -0700

    added comments to prepare for user authentication

--- src/store.js
+++ src/store.js
@@ -1,7 +1,21 @@
 import {createStore, applyMiddleware} from 'redux';
-import {recipeReducer} from './reducers/index.js';
+//import {reducer as formReducer} from 'redux-form';
 import thunk from 'redux-thunk';
-
+//import {loadAuthToken} from './local-storage'; //look into this file
+import {recipeReducer} from './reducers/index.js';
+//import authReducer from // TODO make auth reducer
+//import protectedDataReducer from //TODO make protected data reducer
+//import {setAuthToken, refreshAuthToken} from //TODO make auth actions
 const store = createStore(recipeReducer, applyMiddleware(thunk))
+
+//const store = createStore(
+//  combineReducers({
+//    form: formReducer,
+//    auth: authReducer,
+//    protectedData: protectedDataReducer,
+//    recipe: recipeReducer
+//  }),
+//  applyMiddleware(thunk)
+//);
 
 export default store;
