60f2f325527c4c5661fc01fe06799d2d93652909
Colin Rupp <colin@thinkful.com>
2018-10-18 12:02:06 -0700

    added reducer and initial state to recipe book reducer

--- src/reducers/recipe-book.js
+++ src/reducers/recipe-book.js
@@ -1,5 +1,9 @@
 //import actions
-import {}
+import {
+  FETCH_U_RECIPES_REQUEST,
+  FETCH_U_RECIPES_SUCCES,
+  FETCH_U_RECIPES_ERROR
+} from '../actions/recipe-book.js';
 
 //set initial state
 const initialState = {
@@ -10,3 +14,22 @@ const initialState = {
 }
 
 //write recipeBookReducer
+export const recipeBookReducer = (state = initialState, action) => {
+  if (action.type === FETCH_U_RECIPES_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true
+    })
+  } else if (action.type === FETCH_U_RECIPES_SUCCES) {
+    return Object.assign({}, state, {
+      loading: false,
+      userRecipes = action.recipes
+    })
+  } else if (action.type === FETCH_U_RECIPES_ERROR) {
+    return Object.assign({}, state, {
+      loading: false,
+      error: action.error
+    })
+  } else {
+    return state
+  }
+}
