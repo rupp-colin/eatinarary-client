847353f03d8bc44e4d6e33748185330aeabfc84d
Colin Rupp <colin@thinkful.com>
2018-10-18 11:35:17 -0700

    added synchronous actions for fetching user recipes

--- src/actions/recipe-book.js
+++ src/actions/recipe-book.js
@@ -1,9 +1,21 @@
 import { API_BASE_URL } from '../config.js';
 
 export const FETCH_U_RECIPES_REQUEST = 'FETCH_U_RECIPES_REQUEST';
+export const fetchURecipesRequest = () => ({
+  type: FETCH_U_RECIPES_REQUEST
+});
 
 export const FETCH_U_RECIPES_SUCCES = 'FETCH_U_RECIPES_SUCCES';
+//should return an array of recipes
+export const fetchURecipesSUCCESS = (recipes) =>({
+  type: FETCH_U_RECIPES_SUCCES,
+  recipes
+});
 
 export const FETCH_U_RECIPES_ERROR = 'FETCH_U_RECIPES_ERROR';
+export const fetchURecipesERROR = (error) => ({
+  type: FETCH_U_RECIPES_ERROR,
+  error
+})
 
 
