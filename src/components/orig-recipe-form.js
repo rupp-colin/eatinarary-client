43ec6e7fbf8b5336a8a3296e34a49dc5fb4c3b91
Colin Rupp <colin@thinkful.com>
2018-10-19 12:12:09 -0700

    began building original recipe submission form

---
+++ src/components/orig-recipe-form.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import {Field, reduxForm, focus} from 'redux-form';
+import Input from './input.js';
+
+export class OriginalRecipeForm extends React.Component {
+
+  onSubmit(values) {
+    const {
+      label,
+      healthLabels,
+      ingredientLines,
+      instructions,
+      source,
+      url,
+      image
+    } = values;
+    const newItem = {
+      label,
+      healthLabels,
+      ingredientLines,
+      instructions,
+      source,
+      url,
+      image
+    }
+    return this.props
+      .dispatch(addOriginalRecipe())
+  }
+
+  render() {
+    return (
+      <form
+        className="login-form"
+        onSubmit={this.props.handleSubmit(values => {
+          this.onSubmit(values);
+        })}>
+        <label htmlFor="label">Title</label>
+        <Field
+          component={Input}
+          type="text"
+          name="label"
+          validate={[]}
+        />
+        <label htmlFor="healthLabels">Health Labels</label>
+        <Field
+          component={Input}
+          type=
+          name="healthLabels"
+          validate={[]}
+        />
+        <label htmlFor="ingredientLines">Ingredients</label>
+        <Field
+          component={Input}
+          type=
+          name="ingredientLines"
+          validate={[]}
+        />
+        <label htmlFor="instructions">Instructions</label>
+        <Field
+          component={Input}
+          type="text"
+          name="instructions"
+          validate={[]}
+        />
+        <label htmlFor="source">Source</label>
+        <Field
+          component={Input}
+          type="text"
+          name="source"
+          validate={[]}
+        />
+        <label htmlFor="url">Original Url</label>
+        <Field
+          component={Input}
+          type="text"
+          name="url"
+          validate={[]}
+        />
+        <label htmlFor="image">Link to an Image</label>
+        <Field
+          component={Input}
+          type="text"
+          name="image"
+          validate={[]}
+        />
+        <button
+          type="submit"
+          disabled={this.props.pristine || this.props.submitting}>
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'addOriginalRecipe',
+  onSubmitFail: (errors, dispatch) => {
+    dispatch(focus('addOriginalRecipe', Object.keys(errors)[0]))
+  }
+})(OriginalRecipeForm)
