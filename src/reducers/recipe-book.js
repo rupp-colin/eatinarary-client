cf485e16f1127161e1865553e19931fe5df5027c
Colin Rupp <colin@thinkful.com>
2018-10-19 13:38:00 -0700

    added reducer logic for submitting original recipe

--- src/reducers/recipe-book.js
+++ src/reducers/recipe-book.js
@@ -1,14 +1,9 @@
 //import actions
 import {
-  FETCH_U_RECIPES_REQUEST,
-  FETCH_U_RECIPES_SUCCES,
-  FETCH_U_RECIPES_ERROR,
-  ADD_RECIPE_REQUEST,
-  ADD_RECIPE_SUCCESS,
-  ADD_RECIPE_ERROR,
-  DELETE_RECIPE_REQUEST,
-  DELETE_RECIPE_SUCCESS,
-  DELETE_RECIPE_ERROR
+  FETCH_U_RECIPES_REQUEST, FETCH_U_RECIPES_SUCCES, FETCH_U_RECIPES_ERROR,
+  ADD_RECIPE_REQUEST, ADD_RECIPE_SUCCESS, ADD_RECIPE_ERROR,
+  DELETE_RECIPE_REQUEST, DELETE_RECIPE_SUCCESS, DELETE_RECIPE_ERROR,
+  ORIGINAL_RECIPE_REQUEST, ORIGINAL_RECIPE_SUCCESS, ORIGINAL_RECIPE_ERROR
 } from '../actions/recipe-book.js';
 
 //set initial state
@@ -19,8 +14,11 @@ const initialState = {
   isHidden: true
 }
 
-//write recipeBookReducer
+//write recipeBookReducer -- should handle all the actions that
+//take place within the /myrecipes route
+
 export const recipeBookReducer = (state = initialState, action) => {
+  //gets all of the users saved recipes from mongoDB
   if (action.type === FETCH_U_RECIPES_REQUEST) {
     return Object.assign({}, state, {
       loading: true,
@@ -36,7 +34,11 @@ export const recipeBookReducer = (state = initialState, action) => {
       loading: false,
       error: action.error
     })
-  } else if (action.type === ADD_RECIPE_REQUEST) {
+  }
+
+  //handles requests to add a recipe from the main search
+  //page to the user
+  else if (action.type === ADD_RECIPE_REQUEST) {
     return Object.assign({}, state, {
       loading: true,
       error: null
@@ -51,7 +53,10 @@ export const recipeBookReducer = (state = initialState, action) => {
       loading: false,
       error: action.error
     })
-  } else if (action.type === DELETE_RECIPE_REQUEST) {
+  }
+
+  //deletes a recipe from the user's recipe book
+  else if (action.type === DELETE_RECIPE_REQUEST) {
     return Object.assign({}, state, {
       loading: true,
       error: null
@@ -68,7 +73,28 @@ export const recipeBookReducer = (state = initialState, action) => {
       loading: false,
       error: action.error
     })
-  }else {
+  }
+
+  //adds a recipe submitted from the "add original
+  //recipe" button on the /myrecipes page
+  else if (action.type === ORIGINAL_RECIPE_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true,
+      error: null
+    })
+  } else if (action.type === ORIGINAL_RECIPE_SUCCESS) {
+    return Object.assign({}, state, {
+      loading: false,
+      userRecipes: [action.recipe, ...state.userRecipes]
+    })
+  } else if (action.type === ORIGINAL_RECIPE_ERROR) {
+    return Object.assign({}, state, {
+      loading: false,
+      error: action.error
+    })
+  }
+
+  else {
     return state
   }
 }
